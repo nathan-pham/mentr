e08329241080e6d3c15225eff794ef35ea5bb077
Nathan Pham <nathanpham.me@gmail.com>
2021-10-09 13:57:02 -0700

    sign up page

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -9,7 +9,7 @@ export default function Signin() {
             
             <div className="flex items-stretch">
 
-                <div className="px-32 w-1/2 h-screen">
+                <div className="px-32 w-1/2 min-h-screen">
 
                     <div className="flex items-center mt-10">
 
@@ -19,24 +19,24 @@ export default function Signin() {
                     </div>
 
                     <h1 className="font-bold text-3xl text-gray-700 mt-16">Sign in</h1>
-                    <p className="text-gray-500 mt-2">Sign in to start learning from qualified mentors</p>
+                    <p className="text-gray-500 mt-2">Log in to start learning from qualified mentors</p>
 
-                    <form className="flex flex-col mt-6">
+                    <form className="flex flex-col mt-8">
 
                         <Input name="email" label="Your email" placeholder="name@domain.com" required />
-                        <Input name="password" label="Password" placeholder="at least 8 characters" required className="mt-6" />
+                        <Input name="password" label="Password" placeholder="at least 8 characters" required className="mt-4" />
                         
-                        <button className="px-3 py-2 bg-blue-500 text-white rounded-lg mt-6">Log In</button>
+                        <button className="px-3 py-2 bg-blue-500 text-white rounded-lg mt-4">Log In</button>
 
-                        <p className="mt-6 self-end text-gray-500">Don't have an account? <a href="/auth/signup" className="text-blue-500 font-medium">Sign up</a></p>
+                        <p className="self-end text-gray-500 mt-4">Don't have an account? <a href="/auth/signup" className="text-blue-500 font-medium">Sign up</a></p>
 
                     </form>
 
                 </div>
 
-                <div className="flex items-center justify-center w-1/2 h-screen bg-blue-50">
+                <div className="flex items-center justify-center w-1/2 min-h-screen bg-blue-50">
 
-                    <img src="/undraw_coder.svg" className="w-8/12" />
+                    <img src="/undraw/proud.svg" className="w-8/12" />
 
                 </div>
                 
--- pages/auth/signup.js
+++ pages/auth/signup.js
@@ -1,6 +1,7 @@
 import Root from "@components/Root"
+import Input from "@components/auth/Input"
 
-export default function Signup() {
+export default function signUp() {
 
     return (
 
@@ -8,37 +9,37 @@ export default function Signup() {
             
             <div className="flex items-stretch">
 
-                <div className="flex flex-col justify-center px-32 w-1/2 h-screen">
+                <div className="px-32 w-1/2 min-h-screen">
 
-                    <div className="flex items-center">
+                    <div className="flex items-center mt-10">
 
                         <img src="/icons/mentr.png" alt="Mentr Logo" className="w-14 h-14 mr-2" />
                         <h1 className="text-gray-700 text-xl">mentr</h1>
 
                     </div>
 
-                    <h1 className="font-bold text-3xl text-gray-700 mt-10">Sign in</h1>
-                    <p className="text-gray-500 mt-2">Sign in to start learning from qualified mentors</p>
+                    <h1 className="font-bold text-3xl text-gray-700 mt-16">Sign up</h1>
+                    <p className="text-gray-500 mt-2">Create a free account to get full access to Mentr</p>
 
-                    <form className="flex flex-col mt-8">
+                    <form className="flex flex-col mt-6">
 
-                        <label for="email" className="text-gray-500 font-medium">Your email</label>
-                        <input placeholder="name@domain.com" name="email" type="email" className="border border-gray-400 mt-2 rounded-lg px-3 py-2 outline-none focus:border-blue-500" required />
-
-                        <label for="password" className="text-gray-500 font-medium mt-6">Password</label>
-                        <input placeholder="at least 8 characters" name="password" type="password" className="border border-gray-400 mt-2 rounded-lg px-3 py-2 outline-none focus:border-blue-500" required />
+                        <Input name="email" label="Your name" placeholder="full name" required />
+                        <Input name="email" label="Your email" placeholder="name@domain.com" required className="mt-4" />
+                        <Input name="password" label="Password" placeholder="at least 8 characters" required className="mt-4" />
                         
-                        <button className="px-3 py-2 bg-blue-500 text-white rounded-lg mt-6">Log In</button>
+                        <button className="px-3 py-2 bg-blue-500 text-white rounded-lg mt-4">Register</button>
 
-                        <p className="mt-8 self-end text-gray-500">Don't have an account? <a href="/auth/signup" className="text-blue-500 font-medium">Sign up</a></p>
+                        <p className="self-end text-gray-500 mt-4">Have an account? <a href="/auth/signin" className="text-blue-500 font-medium">Sign in</a></p>
 
                     </form>
 
+                    <footer className="h-10"></footer>
+
                 </div>
 
-                <div className="flex items-center justify-center w-1/2 h-screen bg-gray-100">
+                <div className="flex items-center justify-center w-1/2 min-h-screen bg-blue-50">
 
-                    <img src="/undraw_coder.svg" className="w-8/12" />
+                    <img src="/undraw/pair.svg" className="w-8/12" />
 
                 </div>
                 
