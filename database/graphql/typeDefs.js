33453fe0f5d09ea649fd0cc488ae238134934464
Nathan Pham <nathanpham.me@gmail.com>
2021-10-09 17:10:18 -0700

    plan gql server

--- database/graphql/typeDefs.js
+++ database/graphql/typeDefs.js
@@ -2,16 +2,60 @@ import { gql } from "apollo-server-micro"
 
 export const typeDefs = gql`
 
+    type Snippet {
+
+        language: String!
+        code: String!
+
+    }
+
+    type Course {
+
+        image: String!
+        title: String!
+        from: String!
+
+    }
+
     type User {
+
         key: ID!
+
+        image: String!
         name: String!
-        email: String!
-        password: String!
-        token: String
+        job: String!
+        university: String!
+        karma: Int!
+        tags: [String]!
+        dates: [String]!
+        experience: [Experience]!
+
+        # TODO: add reviews
+
+        auth: {
+
+            email: String!
+            password: String!
+            token: String
+
+        }
+
+    }
+
+    type Experience {
+
+        image: String!
+        title: String!
+        company: String!
+        dates: String!
+
     }
 
     type Query {
+
         Hello: String!
+        # createUser(name: String!, email: String!, password: String!): User!
+
     }
 
 `
